16d5593e30868b7b84e5f8199abd8eb44916bceb
Karina Hristova <52160729+karinaghristova@users.noreply.github.com>
2022-12-09 23:38:05 +0200

    Server now works with multiple clients

--- client.py
+++ client.py
@@ -2,23 +2,24 @@ import socket
 
 def client_program():
     host = socket.gethostname()
-    port = 5000  # socket server port number
+    port = 5000  
 
     client_socket = socket.socket()
-    client_socket.connect((host, port))  # connect to the server
+    print("Waiting to connect to the server...")
+    client_socket.connect((host, port))  
 
-    message = input(" -> ")
+    message = ''
 
     while message.lower().strip() != 'bye':
-        client_socket.send(message.encode())
         data = client_socket.recv(2048).decode()
 
         if 'bye' in data.lower().strip():
             print('Server: ' + data.replace('bye', ''))
             break
+        
         print('Server: ' + data)
-
         message = input(" -> ")
+        client_socket.send(message.encode())
 
     client_socket.close()
 
--- server.py
+++ server.py
@@ -1,32 +1,52 @@
 import socket
 import re
 from quicksort import quicksort as sort
+from _thread import *
+from datetime import datetime
 
 def server_program():
     host = socket.gethostname()
     port = 5000 
+    clients_count = 0
 
     server_socket = socket.socket()
-    server_socket.bind((host, port))  # binding host address and port together
+    server_socket.bind((host, port))
 
     server_socket.listen()
-    conn, address = server_socket.accept()  # accepting new connection
-    print("Connection made from: " + str(address))
-    arr = []
-    while True:
-        data = conn.recv(2048).decode()
-        arr = re.findall(r'[-+]?[.]?[\d]+(?:,\d\d\d)*[\.]?\d*(?:[eE][-+]?\d+)?', data)
-       
-        if len(arr) != 0:  #won't stop until the client gives numbers to be sorted
-            print("Client: " + ' '.join(arr))
-            sort(arr, 0, len(arr) - 1)
-            print("Sorted numbers: " + ' '.join(arr))
-            conn.send(("Sorted numbers: " + ' '.join(arr) + " bye").encode())
+    print("Waiting for clients to connect...")
+
+    def multi_thread_client(conn, count):
+        conn.send(('Successfully connected to server...\nPlease enter the numbers you want to sort').encode())
+
+        while True:
+            arr = []
+            data = conn.recv(2048).decode()
+            arr = re.findall(r'[-+]?[.]?[\d]+(?:,\d\d\d)*[\.]?\d*(?:[eE][-+]?\d+)?', data)
+
+            if 'bye' in data.lower().strip() or not data:
+                print('Connection with client ' + str(count) + " ended")
+                break
+
+            if len(arr) != 0:
+                print("Unsorted numbers of client " + str(count) + ": " + ' '.join(arr))
+                sort(arr, 0, len(arr) - 1)
+                print("Sorted numbers of client " + str(count) + ': ' + ' '.join(arr))
+                conn.send(("Sorted numbers: "+ ' '.join(arr) + " bye").encode())
+                break
+            print("Client " + str(count) + ": " + str(data))
+            conn.send(('Please enter the numbers you want to sort').encode())
+
+        print("Closing connection with client " + str(count))
+        conn.close()  
             
-            break
-        print("Client: " + str(data))
-        conn.send(('Please enter the numbers you want to sort').encode())
-    conn.close()  # close the connection
+    while True:
+        conn, address = server_socket.accept()  
+        print("Connection made from: " + str(address) + ' at ' + str(datetime.now().time()))
+        clients_count += 1
+        start_new_thread(multi_thread_client, (conn, clients_count))
+        print('Number of clients that connected to the server successfully since start: ' + str(clients_count))
+    
+    server_socket.close()
 
 
 if __name__ == '__main__':

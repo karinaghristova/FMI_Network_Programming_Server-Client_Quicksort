bcb64a29e482e4df04d41da7458b243ea8aa6a35
Karina Hristova <52160729+karinaghristova@users.noreply.github.com>
2023-01-10 02:22:03 +0200

    Added parallel version

--- quicksort.py
+++ quicksort.py
@@ -60,10 +60,11 @@ def quicksort_parallel(arr, lowIndex, highIndex):
 
     p1.join()
     p2.join()
-    
+
 
 if __name__ == '__main__':
     list = [156, 1651, -955, 1, 20, 69, 31]
     print(list)
     quicksort(list, 0, len(list)-1)
     print(list)
+
